3c2759afa1b65930d9bc27cd78aeebcc13b49cf9
dzmitrybaranau <dimabaranov98@gmail.com>
2024-05-26 14:01:50 +0200

    feat(create-goal)

--- src/utils/user/saveOnboardingQuestionAnswer.ts
+++ src/utils/user/saveOnboardingQuestionAnswer.ts
@@ -1,42 +1,12 @@
 import { DocumentReference } from "@firebase/firestore";
 import { setUserData } from "./setUserData";
-import {UserAccount} from "airy-help-utils";
+import { UserAccount } from "airy-help-utils";
 
 export const saveOnboardingQuestionAnswer = async ({
-  category,
-  replyMessage,
-  user,
-  userRef,
-  text,
+
 }: {
   userRef: DocumentReference;
   user: UserAccount;
   category: string;
-  replyMessage: string;
   text: string;
-}) => {
-  await setUserData({
-    userRef,
-    newUserData: {
-      ...user,
-      onboardingQuestions: {
-        ...user.onboardingQuestions,
-        // @ts-ignore
-        questions: {
-          ...user.onboardingQuestions?.questions,
-          [category]: user.onboardingQuestions?.questions[category].map(
-            (question: { question: string }) => {
-              if (question.question === replyMessage) {
-                return {
-                  ...question,
-                  userReply: text,
-                };
-              }
-              return question;
-            },
-          ),
-        },
-      },
-    },
-  });
-};
+}) => {};
