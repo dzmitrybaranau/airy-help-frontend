d92813dd2e0880eb42f176e89d562c04585f647e
dzmitrybaranau <dimabaranov98@gmail.com>
2024-07-26 22:55:50 +0300

    feat(logs)

--- src/hooks/useLoadUserAccount.ts
+++ src/hooks/useLoadUserAccount.ts
@@ -5,7 +5,9 @@ const useLoadUserAccount = () => {
   const { userTmaInfo, isTmaInfoLoading, fetchUserAccount } = useUserStore();
 
   useLayoutEffect(() => {
+    console.log("LOAD ACCOUNT");
     if (!isTmaInfoLoading && userTmaInfo?.user?.id) {
+      console.log("CREATE ACCOUNT", userTmaInfo?.user?.id.toString());
       fetchUserAccount(userTmaInfo?.user?.id.toString());
     }
   }, [isTmaInfoLoading, userTmaInfo, fetchUserAccount]);
